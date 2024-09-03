a9c8d59a65ac98df2995841d569e363ba8f954bc
Joan Puigcerver <joapuiib@gmail.com>
2024-09-03 16:50:18 +0200

    text colors css

--- run.sh
+++ run.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
+function print {
+    GREEN="\033[0;32m"
+    RESET="\033[0m"
+    echo -e "${GREEN}$1${RESET}"
+}
+
 BUILD=0
 INSTALL_VENV=0
+ARGS=''
 while [ $# -gt 0 ] ; do
     case $1 in
         -b)
             BUILD=1
-            CONTAINER_NAME="itb_build"
             ;;
         --install-venv)
             INSTALL_VENV=1
+            ;;
+        *)
+            ARGS="$ARGS $1"
             ;;
     esac
     shift
 done
 
-if [ $INSTALL_VENV -eq 1 ]; then
-    echo "Installing virtual environment"
-    python3 -m venv venv
-    echo "Installing dependencies"
-    ./venv/bin/pip install -r requirements.txt
-    echo "Installing this package"
-    ./venv/bin/pip install -e .
+if [ ! -d "venv" ]; then
+    INSTALL_VENV=1
+    print "Virtual environment not found."
 fi
 
-COMMAND="serve --watch-theme"
+if [ $INSTALL_VENV -eq 1 ]; then
+    if [ -d "venv" ]; then
+        print "Removing existing virtual environment..."
+        rm -rf venv
+    fi
+
+    print "Installing virtual environment..."
+    python3 -m venv venv
+    print "Installing dependencies"
+    ./venv/bin/pip install -r requirements.txt
+fi
+
+COMMAND="serve"
 if [ $BUILD -eq 1 ]; then
     COMMAND="build"
 fi
 
-./venv/bin/mkdocs $COMMAND
+./venv/bin/mkdocs $COMMAND $ARGS
