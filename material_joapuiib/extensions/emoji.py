a91c3d13b5c6505f02350c8746428d11427ec48c
Joan Puigcerver <joapuiib@gmail.com>
2024-09-13 12:25:01 +0200

    fix: emoji extension due previos folder reestructure

--- material_joapuiib/extensions/emoji.py
+++ material_joapuiib/extensions/emoji.py
@@ -3,13 +3,13 @@ import os
 from inspect import getfile
 from markdown import Markdown
 from material.extensions.emoji import _load_twemoji_index
-import material_joapuiib
+import material_joapuiib.templates as templates
 
 # Create twemoji index
 def twemoji(options: object, md: Markdown):
     paths = options.get("custom_icons", [])[:]
 
-    root = os.path.dirname(getfile(material_joapuiib))
+    root = os.path.dirname(getfile(templates))
     root = os.path.join(root, ".icons")
 
     paths.append(root)
